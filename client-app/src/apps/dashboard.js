f2ae8476fcac6c9a4c38f497fd548d667ed7553b
haynesjm42 <jm.haynes.42@gmail.com>
2019-08-29 07:50:16 -0400

    Use child window container

--- client-app/src/apps/dashboard.js
+++ client-app/src/apps/dashboard.js
@@ -2,13 +2,15 @@ import {XH} from '@xh/hoist/core';
 import {AppContainer} from '@xh/hoist/desktop/appcontainer';
 import {App} from '../dashboard/App';
 import {AppModel} from '../dashboard/AppModel';
+import {isRunningInOpenFin, getWindow} from '@xh/hoist/openfin/utils';
+import {ChildWindowAppContainer} from '@xh/hoist/openfin/appcontainer';
 
 XH.renderApp({
     clientAppCode: 'dashboard',
     clientAppName: 'Dashboard',
     componentClass: App,
     modelClass: AppModel,
-    containerClass: AppContainer,
+    containerClass: getContainerClass(),
     isMobile: false,
     isSSO: false,
     webSocketsEnabled: true,
@@ -16,3 +18,12 @@ XH.renderApp({
     checkAccess: 'APP_READER',
     loginMessage: 'User: \'toolbox@xh.io\' / Password: \'toolbox\''
 });
+
+function getContainerClass() {
+    if (isRunningInOpenFin()) {
+        const isMainWnd = getWindow().isMainWindow();
+        return isMainWnd ? AppContainer : ChildWindowAppContainer;
+    }
+
+    return AppContainer;
+}
